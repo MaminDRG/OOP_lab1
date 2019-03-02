138d53d15d9f15e34caf972b8741765762eb8baa
MaminDRG <artyomka50@gmail.com>
2019-03-02 22:23:25 +0300

    working ver 1.0.

---
+++ OOP_lab1/number.h
@@ -0,0 +1,14 @@
+//
+//  number.h
+//  lab1
+//
+//  Created by MacBook Pro on 2/22/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+#ifndef number_h
+#define number_h
+
+typedef double number;
+
+#endif /* number_h */
---
+++ OOP_lab1/polinom.cpp
@@ -0,0 +1,55 @@
+//
+//  polinom.cpp
+//  lab1
+//
+//  Created by MacBook Pro on 2/22/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+#include "polinom.h"
+#include "cmath"
+TPolinom::TPolinom(number x,number y,number z)
+{
+    a = x;
+    b = y;
+    c = z;
+}
+
+number TPolinom::value(number x)
+{
+    return (a*x*x+b*x+c);
+}
+
+int TPolinom::roots(number* x)
+{
+    int ret = 0;
+    number determ = b*b-4*a*c;
+    if (determ == 0)
+    {
+        x[0] = (-b)/(2*a);
+        ret = 1;
+    }
+    else if (determ > 0)
+    {
+        x[0] = (-b+sqrt(determ))/(2*a);
+        x[1] = (-b-sqrt(determ))/(2*a);
+        ret = 2;
+    }
+    else ret = 0;
+    
+    return ret;
+}
+
+std::ostream& operator<<(std::ostream& os, TPolinom& p)
+{
+
+    os<<std::endl << "p(x) = " << p.a << "x^2";
+    
+    if (p.b<0) os<<p.b<<"x";
+    else os<<"+"<<p.b<<"x";
+    
+    if (p.c<0) os<<p.c;
+    else os<<"+"<<p.c;
+    //...
+    return os;
+}
